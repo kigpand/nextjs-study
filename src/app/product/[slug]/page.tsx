3dad6fd2c08e006e97645a8418b718bbe5cb8581
김지훈 <kigpand@gmail.com>
2023-03-21 15:01:37 +0900

    not found

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 type Props = {
   params: {
     // 생성한 키워드 이름
@@ -6,5 +8,15 @@ type Props = {
 };
 
 export default function Pants({ params }: Props) {
+  if (params.slug === "nothing") {
+    notFound();
+  }
   return <div>{params.slug}</div>;
+}
+
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({
+    slug: product,
+  }));
 }
---
+++ src/app/product/not-found.tsx
@@ -0,0 +1,3 @@
+export default function ProductNotFound() {
+  return <div>찾을수가 없슴다</div>;
+}
