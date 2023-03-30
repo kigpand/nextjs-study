9e58e6ba0562748b8373ca3a8904a2a7f79db296
김지훈 <kigpand@gmail.com>
2023-03-30 22:48:49 +0900

    redirect 처리 기록

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "test",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/products/delete_forever",
+        destination: "/products",
+        permanent: true,
+      },
+      {
+        source: "/products/delete_temped",
+        destination: "/products",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
